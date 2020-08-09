11840bf7836ffdcc34ff72e3dc8ea1c2b1f44281
Faraheng <farahengnajib@gmail.com>
2020-08-10 00:00:41 +0800

    backend change

---
+++ server.js
@@ -0,0 +1,24 @@
+console.log('May Node be with you')
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
+
+app.listen(3000, function () {
+    console.log('listening on 3000')    
+})
