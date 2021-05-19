7d3ca949f7ea905e2a72c69e1b1bf5fb920b2097
FarahNajib <farahengnajib@gmail.com>
2021-05-19 11:24:36 +0800

    change

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-console.log('May Node be with you')
+console.log('server start ...')
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 
 
-
-
+//setting middleware
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('public')); 
+var publicDir = require('path').join(__dirname,'/images');  
+app.use('/images', express.static(publicDir));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
